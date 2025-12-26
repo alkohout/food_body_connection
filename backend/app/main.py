22c3d4d270fa239dae2133628c2c0e541f46d4cc
alkohout <kohoutal@gmail.com>
2025-12-26 18:33:58 +1300

    https

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://alkohout.github.io",
+        "*",
     ],
     allow_credentials=True,
     allow_methods=["*"],
