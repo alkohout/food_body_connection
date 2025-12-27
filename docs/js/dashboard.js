8cb31a32f36fd4bd3bb4693c70081d391b821cae
alkohout <kohoutal@gmail.com>
2025-12-28 11:33:47 +1300

    createallergen

--- docs/js/dashboard.js
+++ docs/js/dashboard.js
@@ -15,32 +15,4 @@ async function init() {
   }
 }
 
-document.getElementById("log-form").addEventListener("submit", async (e) => {
-  e.preventDefault(); // stops page reload
-
-  const token = localStorage.getItem("access_token");
-
-  const payload = {
-    allergen_id: parseInt(document.getElementById("allergen-item").value),
-    date_time: document.getElementById("entry-date").value
-  };
-
-  const res = await fetch(`${API_URL}/entries`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`
-    },
-    body: JSON.stringify(payload)
-  });
-
-  if (!res.ok) {
-    document.getElementById("log-error").textContent = "Failed to log entry";
-    return;
-  }
-
-  document.getElementById("log-success").textContent = "Entry logged!";
-});
-
-
 init();
