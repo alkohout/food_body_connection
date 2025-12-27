4e70783fb3c25b0c60859457a69fd008da314afb
alkohout <kohoutal@gmail.com>
2025-12-28 07:57:33 +1300

    createallergen

--- backend/app/api/routes/entries.py
+++ backend/app/api/routes/entries.py
@@ -2,22 +2,27 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.models.table_class import Entry
 from app.api.routes.auth import get_current_user
-from app.schemas.entry import EntryCreate
+from app.schemas.entry import AllergenEntryCreate, EntryRead
+from app.models.table_class import AllergenLog
 
 router = APIRouter(prefix="/entries", tags=["entries"])
 
-@router.post("", response_model=EntryCreate)
-def create_entry(entry: EntryCreate, db: Session = Depends(get_db), current_user=Depends(get_current_user)):
-    new_entry = Entry(
+@router.post("/", response_model=EntryRead)
+def allergen_entry_create(
+    entry: AllergenEntryCreate,
+    db: Session = Depends(get_db),
+    current_user=Depends(get_current_user)
+):
+    new_log = AllergenLog(
         user_id=current_user.user_id,
-        food=entry.food,
-        symptom=entry.symptom,
-        date=entry.date
+        date_time=entry.date_time,
+        allergen_id=entry.allergen_id,
+        quantity = entry.quantity,
+        unit_id = entry.unit_id
     )
-    db.add(new_entry)
+    db.add(new_log)
     db.commit()
-    db.refresh(new_entry)
-    return new_entry
+    db.refresh(new_log)
+    return new_log
 
