815768d23df61bcb2edc5ed894ac99b26e66577b
alkohout <kohoutal@gmail.com>
2025-12-28 11:30:28 +1300

    createallergen

--- backend/app/api/routes/entries.py
+++ backend/app/api/routes/entries.py
@@ -5,24 +5,3 @@ from app.database import get_db
 from app.api.routes.auth import get_current_user
 from app.schemas.entry import AllergenEntryCreate
 from app.models.table_class import AllergenLog
-
-router = APIRouter(prefix="/entries", tags=["entries"])
-
-@router.post("", response_model=EntryRead)
-def allergen_entry_create(
-    entry: AllergenEntryCreate,
-    db: Session = Depends(get_db),
-    current_user=Depends(get_current_user)
-):
-    new_log = AllergenLog(
-        user_id=current_user.user_id,
-        date_time=entry.date_time,
-        allergen_id=entry.allergen_id,
-        quantity = entry.quantity,
-        unit_id = entry.unit_id
-    )
-    db.add(new_log)
-    db.commit()
-    db.refresh(new_log)
-    return new_log
-
--- backend/app/schemas/entry.py
+++ backend/app/schemas/entry.py
@@ -4,8 +4,4 @@ from pydantic import BaseModel
 from datetime import date, datetime
 from typing import Optional
 
-class AllergenEntryCreate(BaseModel):
-    date_time: datetime
-    allergen_id: int
-    quantity: Optional[float] = None
-    unit_id: Optional[int] = None
+
