6cad4ecc5c4f5de9119a7f9126ecd1b4b1236877
alkohout <kohoutal@gmail.com>
2025-12-29 18:53:21 +1300

    seed

--- backend/scripts/seed_data.py
+++ backend/scripts/seed_data.py
@@ -2,7 +2,8 @@ import os
 import sys
 
 # Ensure project root is in sys.path so 'app' can be imported
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+sys.path.insert(0, project_root)
 
 from datetime import datetime, timezone
 from app.models.table_class import User, AllergenLog, SymptomLog, Allergen, Unit, Symptom
