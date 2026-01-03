67cbd6ed815188c2c303495ddbf12de7724f48dc
alkohout <kohoutal@gmail.com>
2026-01-04 06:03:56 +1300

    intensity plot to df

--- backend/app/api/routes/plot_eda.py
+++ backend/app/api/routes/plot_eda.py
@@ -7,7 +7,7 @@ from app.api.routes.auth import get_current_user
 from fastapi import HTTPException
 from fastapi.responses import StreamingResponse
 from app.models.table_class import User
-from app.data.analysis_data import get_all_allergen_events, get_all_symptom_events, get_allergen
+from app.data.analysis_data import get_all_allergen_events_df, get_all_symptom_events_df
 from datetime import timedelta, datetime
 import traceback
 
