5e92f0cd44f47a49605d4b65f7af56f82e3d390c
alkohout <kohoutal@gmail.com>
2026-01-06 06:03:57 +1300

    heatmap

--- backend/app/analysis/eda.py
+++ backend/app/analysis/eda.py
@@ -1,9 +1,6 @@
      
 from app.models.table_class import User   
-from fastapi import Depends
 from sqlalchemy.orm import Session
-from app.database import get_db
-from app.api.routes.auth import get_current_user
 from app.schemas.analyse import X,y
 from app.data.analysis_data import get_all_allergen_events_df, get_all_symptom_events_df
 from app.analysis.get_xy import get_xy
@@ -15,13 +12,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns       
 
 def eda_plot_heatmap(
-    db: Session = Depends(get_db),
-    current_user: User = Depends(get_current_user),
-
+    current_user: int,
+    db: Session 
 ):
 
-    allergen_events = get_all_allergen_events_df(db, current_user.user_id)
-    symptom_events = get_all_symptom_events_df(db, current_user.user_id)
+    allergen_events = get_all_allergen_events_df(db, current_user)
+    symptom_events = get_all_symptom_events_df(db, current_user)
 
     X,y = get_xy(allergen_events, symptom_events)
 
