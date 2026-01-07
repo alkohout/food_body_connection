51a658bf318a780a66fba3c49bcf34ef0a53296d
alkohout <kohoutal@gmail.com>
2026-01-07 19:06:27 +1300

    heatmap

--- backend/app/analysis/eda.py
+++ backend/app/analysis/eda.py
@@ -24,7 +24,11 @@ def eda_plot_heatmap(
     symptom_events = get_all_symptom_events_df(db, current_user)
 
     X,y = get_xy(db, allergen_events, symptom_events)
+    print("X columns:", X.columns.tolist())
+    print(X.head())
     X = pd.get_dummies(X, columns=["allergen_name"], drop_first=False)
+    print("X columns:", X.columns.tolist())
+    print(X.head())
 
     summary = (
     pd.concat([X, y], axis=1)
