0230f8bed5437ae680cdcc625532a7cd100f8d9f
alkohout <kohoutal@gmail.com>
2026-01-11 15:04:23 +1300

    allergen rank

---
+++ backend/app/analysis/model.py
@@ -0,0 +1,83 @@
+     
+from app.models.table_class import User   
+from sqlalchemy.orm import Session
+from app.schemas.analyse import X,y
+from app.data.analysis_data import get_all_allergen_events_df, get_all_symptom_events_df
+from app.analysis.get_xy import get_xy
+from app.analysis.supervised_classification import supervised_classification, param_optimization
+from io import BytesIO
+import pandas as pd
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import seaborn as sns       
+import logging
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+logger = logging.getLogger("app/analysis/eda.py")
+logging.basicConfig(level=logging.INFO)
+
+def model_classification(
+    db: Session,
+    current_user: int
+):
+
+    allergen_events = get_all_allergen_events_df(db, current_user)
+    symptom_events = get_all_symptom_events_df(db, current_user)
+
+    X,y = get_xy(db, allergen_events, symptom_events)
+
+    X_train,  X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    model, fpr,tpr,roc_auc = supervised_classification(X,y,method='logistic_regression')
+    lr_params = {
+        'penalty': ['l1','l2'],
+        'C': [1, 10, 100]
+    }
+    best_params = param_optimization(model,lr_params,X_train, y_train, X_test, y_test)
+    model,fpr,tpr,roc_auc = supervised_classification(X,y,method='logistic_regression',params='best_params')
+
+    coefs = model.coef_.ravel()
+
+    allergen_importance = pd.DataFrame({
+        "allergen": X.columns,
+        "coefficient": coefs,
+        "odds_ratio": np.exp(coefs)
+    }).sort_values("coefficient", ascending=False)
+
+
+    # Plot
+    plt.figure(figsize=(10, 6))
+    plot_df = allergen_importance.sort_values("odds_ratio", ascending=True)
+    sns.barplot(
+        data=plot_df,
+        x="odds_ratio",
+        y="allergen"
+    )
+    plt.axvline(1.0, linestyle="--")  # no-effect line
+    plt.xlabel("Odds Ratio (symptoms within 24h)")
+    plt.ylabel("Allergen")
+    plt.title("Allergens Most Likely to Trigger Symptoms")
+    plt.tight_layout()
+
+    # --- Save to PNG ---
+    buf = BytesIO()
+    plt.savefig(buf, format="png", bbox_inches="tight")
+    buf.seek(0)
+
+    return buf
+
+    # agg = (
+    #    df
+    #    .groupby("allergen_name")
+    #    .agg(
+    #        n_exposures=("symptom_occurred", "size"),
+    #        symptom_rate=("symptom_occurred", "mean"),
+    #        mean_intensity=("symptom_max_intensity", "mean"),
+    #    )
+    #agg = agg.sort_values("n_exposures", ascending=False).head(20)
+    #sns.heatmap(
+    #    agg[["symptom_rate", "mean_intensity"]],
+    #    annot=True,
+    #    cmap="coolwarm",
+    #)
---
+++ backend/app/analysis/supervised_classification.py
@@ -0,0 +1,167 @@
+def supervised_classification(X,y,method='logistic_regression',params=None):
+    
+    """ 
+    Perform supervised classification on a dataset with a binary target.
+    """ 
+
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    from sklearn.model_selection import train_test_split
+    from sklearn import metrics
+    from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, auc, classification_report
+
+    if params is None:
+        params = {}  # default to empty dictionary
+
+    X_train,  X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    if method == 'logistic_regression':
+        from sklearn.linear_model import LogisticRegression
+        model = LogisticRegression(**params)
+    elif method == 'svm':
+        from sklearn import svm
+        model = svm.SVC(**params, probability=True)
+    elif method == 'naive_bayes':
+        from sklearn.naive_bayes import GaussianNB
+        model = GaussianNB()
+    elif method == 'decision_trees':
+        from sklearn.tree import DecisionTreeClassifier
+        model = DecisionTreeClassifier(**params)
+    elif method == 'random_forest':
+        from sklearn.ensemble import RandomForestClassifier
+        model = RandomForestClassifier(**params)
+    elif method == 'bagging':
+        from sklearn.ensemble import BaggingClassifier
+        model = BaggingClassifier(**params)
+    elif method == 'boosting':  
+        from sklearn.ensemble import GradientBoostingClassifier
+        model = GradientBoostingClassifier(**params)
+    elif method == 'stacking':
+        from sklearn.ensemble import StackingClassifier
+        from sklearn.linear_model import LogisticRegression
+        from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+
+        estimators = [
+            ('lr', LogisticRegression(max_iter=1000)),
+            ('rf', RandomForestClassifier()),
+        ]
+
+        model = StackingClassifier(
+            estimators=estimators,
+            final_estimator=GradientBoostingClassifier(),
+        )
+    else:
+        raise ValueError("Unsupported method.") 
+
+    model.fit(X_train, y_train)
+
+    # Predictions on test set
+    y_pred = model.predict(X_test)
+    cm = metrics.confusion_matrix(y_test, y_pred)
+    cm_df = pd.DataFrame(cm, 
+                        columns=['predicted 0', 'predicted 1 '],
+                        index=['is 0', 'is 1'])
+    print(cm_df)    
+    print(classification_report(y_test, y_pred))
+    y_pred_prob = model.predict_proba(X_test)
+    y_pred_prob_df = pd.DataFrame(y_pred_prob, columns=['class_0_pp', 'class_1_pp'])
+    # Find fpr, tpr
+    fpr, tpr, _ = metrics.roc_curve(y_test, y_pred_prob_df['class_1_pp'])
+    # Find auc
+    roc_auc = metrics.auc(fpr, tpr)
+    '''
+    # Plot of a ROC curve for class 1 
+    plt.figure(figsize=[8,8])
+    # Plot fpr, tpr
+    plt.plot(fpr, tpr, color='darkorange', lw = 2, label = 'ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], 'k--', linewidth=4)
+    plt.xlim([-0.05, 1.0])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('False Positive Rate', fontsize=18)
+    plt.ylabel('True Positive Rate', fontsize=18)
+    plt.title('Receiver operating characteristic for cancer detection', fontsize=18)
+    plt.legend(loc="lower right")
+    plt.show()
+    '''
+    
+    return model,fpr,tpr,roc_auc
+
+def param_optimization(model,parameters,X_train, y_train, X_test, y_test):
+    """
+    Perform hyperparameter optimization using GridSearchCV.
+    """
+    from sklearn.model_selection import GridSearchCV
+    from sklearn.metrics import accuracy_score,classification_report
+
+    grid = GridSearchCV(model, parameters, cv=5, scoring='accuracy')
+    grid.fit(X_train, y_train)
+    print("Best parameters: ", grid.best_params_)
+    print("Best cross-validation accuracy: ", grid.best_score_)     
+
+    best_model = grid.best_estimator_
+    # Predictions on test set
+    y_pred = best_model.predict(X_test)
+
+    print("Test Accuracy:", accuracy_score(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
+
+    return grid.best_params_
+
+def roc_plot(X,y,models,params):
+
+    import matplotlib.pyplot as plt
+
+    plt.figure(figsize=[8,8])
+    plt.xlim([-0.05, 1.0])
+    plt.ylim([-0.05, 1.05])
+
+    for model in models:
+        mod,fpr,tpr,roc = supervised_classification(X,y,method=model,params=params[model])
+        plt.plot(fpr, tpr, lw = 2, label = '%s ROC curve (area = %0.2f)' % (model,roc))
+
+    plt.xlabel('False Positive Rate', fontsize=18)
+    plt.ylabel('True Positive Rate', fontsize=18)
+    plt.title('Receiver Operating Characteristic: M', fontsize=18)
+    plt.legend(loc="lower right")
+    plt.show()
+
+def predictions(X,y,model):
+    
+    """ 
+    Perform predications on a dataset with a binary target.
+    """ 
+
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    from sklearn import metrics
+    from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, auc, classification_report
+
+    y_pred = model.predict(X)
+    cm = metrics.confusion_matrix(y, y_pred)
+    cm_df = pd.DataFrame(cm, 
+                        columns=['predicted 0', 'predicted 1 '],
+                        index=['is 0', 'is 1'])
+    print(cm_df)    
+    print(classification_report(y, y_pred))
+    y_pred_prob = model.predict_proba(X)
+    y_pred_prob_df = pd.DataFrame(y_pred_prob, columns=['class_0_pp', 'class_1_pp'])
+    # Find fpr, tpr
+    fpr, tpr, _ = metrics.roc_curve(y, y_pred_prob_df['class_1_pp'])
+    # Find auc
+    roc_auc = metrics.auc(fpr, tpr)
+   
+    # Plot of a ROC curve for class 1 
+    plt.figure(figsize=[8,8])
+    # Plot fpr, tpr
+    plt.plot(fpr, tpr, color='darkorange', lw = 2, label = 'ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], 'k--', linewidth=4)
+    plt.xlim([-0.05, 1.0])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('False Positive Rate', fontsize=18)
+    plt.ylabel('True Positive Rate', fontsize=18)
+    plt.title('Receiver operating characteristic', fontsize=18)
+    plt.legend(loc="lower right")
+    plt.show()
+    
+    return model,fpr,tpr,roc_auc
+    
