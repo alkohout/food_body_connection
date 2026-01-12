413e2bf577990e8f943312ca2221da9498330ebc
alkohout <kohoutal@gmail.com>
2026-01-12 20:04:24 +1300

    allergen rank

--- backend/app/analysis/model.py
+++ backend/app/analysis/model.py
@@ -138,8 +138,8 @@ def model_classification(db: 'Session', current_user: int):
 
             # rectangle behind metrics in top-right corner
             rect = Rectangle(
-                (0.85, 0.85 - len(metrics)),  # lower-left corner
-                0.8, 0.8,                        # width, height
+                (0.85, 0.95 - len(metrics)),  # lower-left corner
+                0.15, 0.15,                        # width, height
                 transform=ax.transAxes,
                 color='white',
                 alpha=0.8,
