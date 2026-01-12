d72ce608f865ad8c8e1d7a4332f8d21c2be75a2f
alkohout <kohoutal@gmail.com>
2026-01-12 19:52:49 +1300

    allergen rank

--- backend/app/analysis/model.py
+++ backend/app/analysis/model.py
@@ -135,7 +135,7 @@ def model_classification(db: 'Session', current_user: int):
             plt.gca().add_patch(
             plt.Rectangle(
                 (0.8, 0.8), 0.3, 0.3,
-                transform=ax.transAxes, color='white', alpha=0.2, zorder=0
+                transform=ax.transAxes, color='white', alpha=0.9, zorder=0
             )
 )
 
