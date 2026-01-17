5cf5d1796f2b58ab1784647375d29da73a37ad44
alkohout <kohoutal@gmail.com>
2026-01-18 09:25:50 +1300

    percentages

---
+++ backend/app/api/routes/plot_bar_plots.py
@@ -0,0 +1,47 @@
+# app/api/routes/plot_time_series.py
+import logging
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.database import get_db
+from app.api.routes.auth import get_current_user
+from fastapi import HTTPException
+from fastapi.responses import StreamingResponse
+from app.models.table_class import User
+from app.data.analysis_data import get_all_allergen_events_df, get_all_symptom_events_df
+from app.analysis.eda import plot_percentages 
+from datetime import timedelta, datetime
+import traceback
+from io import BytesIO
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+logger = logging.getLogger("app/api/routes/plot_bar_plots.py")
+logging.basicConfig(level=logging.INFO)
+
+router = APIRouter(prefix="/analysis", tags=["analysis"])
+
+@router.get("/plot_bar_plots")
+def plot_bar_plots(
+        db: Session = Depends(get_db),
+        current_user: User = Depends(get_current_user),
+        allergen_name: str = 'Dairy'
+    ):
+
+    try: 
+
+        buf = bar_percentages(
+            db,
+            current_user.user_id,
+            allergen_name
+        )
+
+        return StreamingResponse(buf, media_type="image/png")
+
+    except Exception as e:
+
+        logger.error("Error generating plot: %s", e)
+        logger.error(traceback.format_exc())
+        raise HTTPException(status_code=500, detail="Failed to generate plot")
