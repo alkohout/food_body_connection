f710336a401577bf1761a4dc9910ac4e50a03066
alkohout <kohoutal@gmail.com>
2026-02-01 18:09:06 +1300

    summary

--- backend/app/api/routes/generate_summary_text.py
+++ backend/app/api/routes/generate_summary_text.py
@@ -34,7 +34,7 @@ def generate_summary_text(
         buf = model_classification(
             db,
             current_user.user_id,
-            return_text="text"
+            return_type="text"
         )
 
         return Response(content=buf, media_type="text/plain")
