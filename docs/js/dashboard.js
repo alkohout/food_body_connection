9c9a4fd97d0e311f59a141ffe902a54ef61e22c9
alkohout <kohoutal@gmail.com>
2026-02-12 12:40:04 +1300

    commenting

--- docs/js/dashboard.js
+++ docs/js/dashboard.js
@@ -1056,6 +1056,7 @@ async function init() {
     const allergenIdInput = getElement("allergen-id");
 
     if (allergenSelect) {
+      console.log({ allergenSelect, allergenInput, allergenIdInput });
       allergenSelect.addEventListener("change", () => {
         const selectedOption = allergenSelect.selectedOptions[0];
 
